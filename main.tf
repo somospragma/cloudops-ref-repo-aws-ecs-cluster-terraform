933c193a333541810c0d0c157df92aa9a67300de
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-01-27 14:47:41 -0500

    Contador name ECS Cluster

--- main.tf
+++ main.tf
@@ -5,33 +5,32 @@
 resource "aws_ecs_cluster" "cluster" {
   provider = aws.project
   for_each = {
-    for item in var.cluster_config : item.application => {
-      containerInsights       = item.containerInsights
-      enableCapacityProviders = item.enableCapacityProviders
-    }
+    for idx, item in var.cluster_config : item.application => merge(item, {
+      index = idx
+    })
   }
-
-  name = lower(join("-", [var.client, var.project, var.environment, "cluster", each.key]))
-
+  name = lower(join("-", [var.client, var.project, var.environment, "cluster", each.key, format("%02d", each.value.index + 1)]))
   setting {
     name  = "containerInsights"
     value = each.value.containerInsights
   }
-
   tags = {
-    Name        = lower(join("-", [var.client, var.project, var.environment, "cluster", each.key, each.value["index"] + 1]))
+    Name        = lower(join("-", [var.client, var.project, var.environment, "cluster", each.key, format("%02d", each.value.index + 1)]))
     Application = each.key
   }
 }
 
+###########################################
+####### ECS Cluster Providers #############
+###########################################
 resource "aws_ecs_cluster_capacity_providers" "cluster_capacity_providers" {
   provider = aws.project
   for_each = {
-    for item in var.cluster_config : item.application => item
+    for idx, item in var.cluster_config : item.application => item
     if item.enableCapacityProviders
   }
 
   cluster_name = aws_ecs_cluster.cluster[each.key].name
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-  depends_on = [ aws_ecs_cluster.cluster ]
+  depends_on = [aws_ecs_cluster.cluster]
 }
